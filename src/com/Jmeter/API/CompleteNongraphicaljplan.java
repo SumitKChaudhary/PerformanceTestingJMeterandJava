ed25be6a301b6246a4e0a0f0d053ddcbe3a6d734
Sumit Kumar Chaudhary <sumitk440@gmail.com>
2020-07-01 16:46:49 +0530

    segregate method and use it

--- src/com/Jmeter/API/CompleteNongraphicaljplan.java
+++ src/com/Jmeter/API/CompleteNongraphicaljplan.java
@@ -21,102 +21,13 @@ import org.apache.jmeter.reporters.ResultCollector;
  import org.apache.jmeter.threads.gui.ThreadGroupGui;  
  import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jmeter.visualizers.ViewResultsFullVisualizer;
-import org.apache.jorphan.collections.HashTree;  
+import org.apache.jorphan.collections.HashTree;
+
+import com.Jmeter.Method.PerformanceTestGeneratJMXandReport;  
    
  public class CompleteNongraphicaljplan {  
    public static void main(String[] argv) throws Exception {  
    
-     //Set jmeter home for the jmeter utils to load  
-     String jmeterHomelocation = "F:\\apache-jmeter-5.2.1\\";  
-     String jmeterPropertieslocation = jmeterHomelocation + "bin\\jmeter.properties";  
-   
-   
-     //JMeter Engine  
-     StandardJMeterEngine jmeter = new StandardJMeterEngine();  
-   
-     //JMeter initialization (properties, log levels, locale, etc)  
-     JMeterUtils.setJMeterHome(new File(jmeterHomelocation).getPath());  
-     JMeterUtils.loadJMeterProperties(new File(jmeterPropertieslocation).getPath());  
-     // see extra log messages of i.e. DEBUG level  
-     JMeterUtils.initLogging();  
-     JMeterUtils.initLocale();  
-   
-     // JMeter Test Plan, basically JOrphan HashTree  
-     HashTree testPlanTree = new HashTree();  
-   
-     // First HTTP Sampler - open google.com  
-     HTTPSampler examplecomSampler = new HTTPSampler();  
-     examplecomSampler.setDomain("127.0.0.1");  
-     examplecomSampler.setPort(3000);  
-     examplecomSampler.setPath("Simple_Json");  
-     examplecomSampler.setMethod("GET");  
-     examplecomSampler.setName("SampleAPI");  
-     examplecomSampler.setProperty(TestElement.TEST_CLASS, HTTPSampler.class.getName());  
-     examplecomSampler.setProperty(TestElement.GUI_CLASS, HttpTestSampleGui.class.getName());  
-   
-    
-     // Loop Controller  
-     LoopController loopCtrl = new LoopController();  
-     loopCtrl.setLoops(10);  
-     loopCtrl.setFirst(true);  
-     loopCtrl.setProperty(TestElement.TEST_CLASS, LoopController.class.getName());  
-     loopCtrl.setProperty(TestElement.GUI_CLASS, LoopControlPanel.class.getName());  
-     loopCtrl.initialize();  
-   
-     
-     // Thread Group  
-     ThreadGroup threadGroup = new ThreadGroup();  
-     threadGroup.setName("Test Thread Group");  
-     threadGroup.setNumThreads(1);  
-     threadGroup.setRampUp(1);  
-     threadGroup.setSamplerController(loopCtrl);  
-     threadGroup.setProperty(TestElement.TEST_CLASS, ThreadGroup.class.getName());  
-     threadGroup.setProperty(TestElement.GUI_CLASS, ThreadGroupGui.class.getName());  
-   
-     // Test Plan  
-     TestPlan testPlan = new TestPlan("Java code Test Plan");  
-     testPlan.setProperty(TestElement.TEST_CLASS, TestPlan.class.getName());  
-     testPlan.setProperty(TestElement.GUI_CLASS, TestPlanGui.class.getName());  
-     testPlan.setUserDefinedVariables((Arguments) new ArgumentsPanel().createTestElement());  
-   
-    
-     // Construct Test Plan from previously initialized elements  
-     testPlanTree.add(testPlan);  
-     HashTree threadGroupHashTree = testPlanTree.add(testPlan, threadGroup);  
-     threadGroupHashTree.add(examplecomSampler);  
-       
-     
-     // save generated test plan to JMeter's .jmx file format  
-     SaveService.saveTree(testPlanTree, new FileOutputStream(System.getProperty("user.dir")+File.separator+"ReportContainer"+File.separator+"jmeter_api_sample2.jmx"));  
-   
-     //add Summarizer output to get test progress in stdout like:  
-     // summary =   2 in  1.3s =  1.5/s Avg:  631 Min:  290 Max:  973 Err:   0 (0.00%)  
-     Summariser summer = null;  
-     String summariserName = JMeterUtils.getPropDefault("summariser.name", "summary");  
-     if (summariserName.length() > 0) {  
-       summer = new Summariser(summariserName);  
-     }  
-   
-   
-    
-     // Store execution results into a .jtl file, we can save file as csv also  
-     String reportFile = System.getProperty("user.dir")+File.separator+"ReportContainer"+File.separator+"summaryreport2.jtl";  
-     String csvFile = System.getProperty("user.dir")+File.separator+"ReportContainer"+File.separator+"summaryreport2.csv";  
-     ResultCollector logger = new ResultCollector(summer);  
-     logger.setFilename(reportFile);  
-     ResultCollector csvlogger = new ResultCollector(summer);  
-     csvlogger.setFilename(csvFile);  
-     testPlanTree.add(testPlanTree.getArray()[0], logger);  
-     testPlanTree.add(testPlanTree.getArray()[0], csvlogger);  
-      
-     
-           // Run Test Plan  
-     jmeter.configure(testPlanTree);  
-     jmeter.run();  
-    
-  
-     System.exit(0);  
-   
-   
+     PerformanceTestGeneratJMXandReport.performanceGenerateTestReport("F:\\apache-jmeter-5.2.1", "F:\\apache-jmeter-5.2.1\\bin\\jmeter.properties", "127.0.0.1", "Simple_Json","GET","SampleTest", "APILoadTestPlan", "JTLTestReportFile", "CSVTestReport");
    }  
  }  
